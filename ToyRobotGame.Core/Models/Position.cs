53050b3bf7afc20f25a90652f16ec6e965abf0bd
luigi059 <luigimarkfernandez@gmail.com>
2025-10-05 20:19:43 +0100

    Added Models

---
+++ ToyRobotGame.Core/Models/Position.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobotGame.Core.Models;
+
+public record Position(int Row, int Col);
---
+++ ToyRobotGame.Core/Models/Robot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToyRobotGame.Core.Models;
+
+public class Robot
+{
+    public Position Position { get; set; }
+    public Direction Facing { get; set; }
+
+    public Robot(Position position, Direction facing)
+    {
+        Position = position;
+        Facing = facing;
+    }
+}
